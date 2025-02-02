ee16571e58638d06ec90144e6329c71d51734e5b
CDanielLG <hikivel139@gmail.com>
2025-02-02 11:36:23 -0600

    updates

--- src/main/java/com/ensolver/springboot/app/notes/controllers/NotesController.java
+++ src/main/java/com/ensolver/springboot/app/notes/controllers/NotesController.java
@@ -43,7 +43,7 @@ public ResponseEntity<String> testAuth() {
 		}
 	    // Obtener todas las notas
 	    @GetMapping
-		@PreAuthorize("hasAnyRole('1')") 
+		@PreAuthorize("hasAnyRole(1)") 
 		public ResponseEntity<List<Note>> getAllNotes() {
 			String userEmail = getAuthenticatedUserEmail(); // Obtén el email del usuario autenticado
 			List<Note> notes = noteService.getNotesByUserEmail(userEmail); // Filtra las notas
@@ -58,7 +58,7 @@ public ResponseEntity<String> testAuth() {
 	    }
 
 	    // Crear una nueva nota
-		@PostMapping("api/notes")
+		@PostMapping
 		@PreAuthorize("hasAnyRole('1')") // Solo usuarios con rol USER o ADMIN pueden crear notas
 		public ResponseEntity<Note> createNote(@RequestBody Note note) {
 			String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
